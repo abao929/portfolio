cad5b732c7768273944740526c0827aeb40bb05e
abao929 <alexbao929@gmail.com>
2022-12-04 16:21:37 -0500

    init files

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import Title from './components/Title'
+import './global.sass'
 
 type Props = {}
 
 export default function Main({}: Props) {
-  return <div>Main</div>
+  return (
+    <div>
+      <Title></Title>
+      <div className='what'>How does this affect things</div>
+      {/* Projects */}
+    </div>
+  )
 }
